0b858a4f9cb7d94f19631f0d1e9d66ece2363f65
Guilhermo97 <146236423+Guilhermo97@users.noreply.github.com>
2025-08-04 11:44:49 -0300

    .

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector('caixa-principal');
+
+const caixaPerguntasl = document.querySelector('caixa-pergunta');
+
+const caixaAlternativas = document.querySelector('caixa-alternativa');
+
+const caixaResultados = document.querySelector('caixa-resultado');
+
+const caixaResultados = document.querySelector('caixa-Resultado');
+
+const lista = [ ];
+
+const pergunta = [ 
+    
+];
